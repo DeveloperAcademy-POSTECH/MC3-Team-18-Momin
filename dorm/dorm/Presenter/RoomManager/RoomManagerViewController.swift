2554ef9d10643178508e003f2d7a622af57ae1e2
HoJong <pjh00098@gmail.com>
2022-07-19 10:44:49 +0900

    Feat: Add RoomManagerViewController

---
+++ dorm/dorm/Presenter/RoomManager/RoomManagerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RoomManagerViewController.swift
+//  dorm
+//
+//  Created by JongHo Park on 2022/07/19.
+//
+
+import UIKit
+
+/// RoomManagerViewController - Room Collection View 와 Room Detail Controller 를 관리하는 ViewController 입니다.
+final class RoomManagerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ 
+}
