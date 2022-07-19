84d55edc222fbb663c20a18a116945493fcceb91
HoJong <pjh00098@gmail.com>
2022-07-19 11:50:26 +0900

    Feat: Add RoomManager UIView

--- dorm/dorm/Presenter/RoomManager/RoomManagerViewController.swift
+++ dorm/dorm/Presenter/RoomManager/RoomManagerViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 /// RoomManagerViewController - Room Collection View 와 Room Detail Controller 를 관리하는 ViewController 입니다.
 final class RoomManagerViewController: UIViewController {
+
+    private lazy var roomManagerView: RoomManagerView = RoomManagerView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
- 
+
+    override func loadView() {
+        view = roomManagerView
+    }
 }
