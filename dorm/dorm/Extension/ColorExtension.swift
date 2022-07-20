76b038386d279872e7fa0e1944039546e2e90382
TaekH <103012087+TaekH@users.noreply.github.com>
2022-07-20 10:13:46 +0900

    Feat: Add Color Extension PostechRed

---
+++ dorm/dorm/Extension/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  dorm
+//
+//  Created by 한택환 on 2022/07/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let postechRed = UIColor(named: "PostechRed")
+}
