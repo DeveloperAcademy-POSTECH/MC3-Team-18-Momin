9f413cd9af5387c424ff839db149c34d3b601340
HoJong <pjh00098@gmail.com>
2022-07-20 10:54:19 +0900

    Feat: Add Custom SplitViewController
    sidebar 영역의 viewController 와, primary 영역의 viewController 를 내부적으로 설정하고 동작할 수 있는 CustomSplitViewController 설정

--- dorm/dorm/System/SceneDelegate.swift
+++ dorm/dorm/System/SceneDelegate.swift
@@ -11,12 +11,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private lazy var splitViewController: CustomSplitViewController = CustomSplitViewController()
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let scene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = UISplitViewController()
+        window?.rootViewController = splitViewController
         window?.windowScene = scene
         window?.makeKeyAndVisible()
     }
