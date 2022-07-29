fb9dba4176683f4dcd01070a0dd79c34e7a570f7
HoJong <pjh00098@gmail.com>
2022-07-29 11:00:24 +0900

    Test: Add student Repository protocol & test

---
+++ dorm/podomTests/PodomTests.swift
@@ -0,0 +1,34 @@
+//
+//  podomTests.swift
+//  podomTests
+//
+//  Created by JongHo Park on 2022/07/29.
+//
+
+@testable import dorm
+import XCTest
+
+final class StudentRepositoryTests: XCTestCase {
+    private var studentRepository: StudentRepository!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testCreateStudents() async throws {
+        // given
+        print("create Students 테스트 시작")
+        let students: [Student] = MockDatas().students
+        let id: String = "test"
+        var result: [Student]?
+        // when
+        result = try await studentRepository.createStudents(id, students)
+        // then
+        XCTAssertEqual(students.count, result?.count)
+    }
+
+}
