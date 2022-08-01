cf85bb669c0ce74cd3fc20e7dc055f17b9aa45a4
italsehf <alsehfdl0316@naver.com>
2022-08-01 16:33:16 +0900

    Refactor: 기존의 RoomCollectionViewCell 코드 가독성을 위해 리팩토링 진행

--- dorm/dorm/Presenter/RoomManager/RoomCollectionViewCell.swift
+++ dorm/dorm/Presenter/RoomManager/RoomCollectionViewCell.swift
@@ -11,47 +11,21 @@ final class RoomCollectionViewCell: UICollectionViewCell {
 
     static let identifier = "RoomCollectionViewCell"
 
+    init() {
+        super.init(frame: .zero)
+        setUpRoomCellViews()
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.addSubview(roomNumberLabel)
-        contentView.addSubview(roundedRectangleView)
-        contentView.addSubview(middleLineView)
-        contentView.addSubview(leftPersonImageView)
-        contentView.addSubview(rightPersonImageView)
-        contentView.addSubview(leftPersonNameLabel)
-        contentView.addSubview(rightPersonNameLabel)
-
-        NSLayoutConstraint.activate([
-            roundedRectangleView.topAnchor.constraint(equalTo: topAnchor, constant: 29),
-            roundedRectangleView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            roundedRectangleView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            roundedRectangleView.trailingAnchor.constraint(equalTo: trailingAnchor),
-
-            middleLineView.topAnchor.constraint(equalTo: roundedRectangleView.topAnchor),
-            middleLineView.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor),
-            middleLineView.centerXAnchor.constraint(equalTo: roundedRectangleView.centerXAnchor),
-            middleLineView.widthAnchor.constraint(equalToConstant: 1),
-
-            leftPersonImageView.centerXAnchor.constraint(equalTo: roundedRectangleView.leadingAnchor, constant: bounds.width / 4),
-            leftPersonImageView.topAnchor.constraint(equalTo: roundedRectangleView.topAnchor, constant: 34),
-            leftPersonImageView.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor, constant: -48),
-
-            rightPersonImageView.centerXAnchor.constraint(equalTo: roundedRectangleView.trailingAnchor, constant: -(bounds.width / 4)),
-            rightPersonImageView.topAnchor.constraint(equalTo: roundedRectangleView.topAnchor, constant: 34),
-            rightPersonImageView.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor, constant: -48),
-
-            leftPersonNameLabel.centerXAnchor.constraint(equalTo: roundedRectangleView.leadingAnchor, constant: bounds.width / 4),
-            leftPersonNameLabel.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor, constant: -9),
-
-            rightPersonNameLabel.centerXAnchor.constraint(equalTo: roundedRectangleView.trailingAnchor, constant: -(bounds.width / 4)),
-            rightPersonNameLabel.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor, constant: -9)
-        ])
+        setUpRoomCellViews()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - UI Component
     private let roomNumberLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
@@ -114,6 +88,59 @@ final class RoomCollectionViewCell: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+}
+
+// MARK: - set up ui components
+extension RoomCollectionViewCell {
+    /// UI 요소를 추가하는 함수
+    func setUpRoomCellViews() {
+        setUpCellView()
+        setUpLabelView()
+        setUpImageView()
+    }
+
+    func setUpCellView() {
+        addSubview(roundedRectangleView)
+        addSubview(middleLineView)
+        NSLayoutConstraint.activate([
+            roundedRectangleView.topAnchor.constraint(equalTo: topAnchor, constant: 29),
+            roundedRectangleView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            roundedRectangleView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            roundedRectangleView.trailingAnchor.constraint(equalTo: trailingAnchor),
+
+            middleLineView.topAnchor.constraint(equalTo: roundedRectangleView.topAnchor),
+            middleLineView.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor),
+            middleLineView.centerXAnchor.constraint(equalTo: roundedRectangleView.centerXAnchor),
+            middleLineView.widthAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+
+    func setUpLabelView() {
+        addSubview(roomNumberLabel)
+        addSubview(leftPersonNameLabel)
+        addSubview(rightPersonNameLabel)
+        NSLayoutConstraint.activate([
+            leftPersonNameLabel.centerXAnchor.constraint(equalTo: roundedRectangleView.leadingAnchor, constant: bounds.width / 4),
+            leftPersonNameLabel.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor, constant: -9),
+
+            rightPersonNameLabel.centerXAnchor.constraint(equalTo: roundedRectangleView.trailingAnchor, constant: -(bounds.width / 4)),
+            rightPersonNameLabel.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor, constant: -9)
+        ])
+    }
+
+    func setUpImageView() {
+        addSubview(leftPersonImageView)
+        addSubview(rightPersonImageView)
+        NSLayoutConstraint.activate([
+        leftPersonImageView.centerXAnchor.constraint(equalTo: roundedRectangleView.leadingAnchor, constant: bounds.width / 4),
+        leftPersonImageView.topAnchor.constraint(equalTo: roundedRectangleView.topAnchor, constant: 34),
+        leftPersonImageView.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor, constant: -48),
+
+        rightPersonImageView.centerXAnchor.constraint(equalTo: roundedRectangleView.trailingAnchor, constant: -(bounds.width / 4)),
+        rightPersonImageView.topAnchor.constraint(equalTo: roundedRectangleView.topAnchor, constant: 34),
+        rightPersonImageView.bottomAnchor.constraint(equalTo: roundedRectangleView.bottomAnchor, constant: -48)
+        ])
+    }
 
     func configureRoomStudents(students: [Student]) {
         leftPersonNameLabel.text = students[0].name
