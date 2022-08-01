7af263ae84822287169cf609df8e40a8fe87da5e
mijinc <mijinc@icloud.com>
2022-08-01 17:00:46 +0900

    Feat: Add RoomEditView and RoomEditViewController

---
+++ dorm/dorm/Presenter/RoomManager/RoomEditView.swift
@@ -0,0 +1,20 @@
+//
+//  RoomEditView.swift
+//  dorm
+//
+//  Created by Mijin CHOI on 2022/08/01.
+//
+
+import SwiftUI
+
+struct RoomEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoomEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomEditView()
+    }
+}
---
+++ dorm/dorm/Presenter/RoomManager/RoomEditViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RoomEditViewController.swift
+//  dorm
+//
+//  Created by Mijin CHOI on 2022/08/01.
+//
+
+import SwiftUI
+
+struct RoomEditViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoomEditViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomEditViewController()
+    }
+}
