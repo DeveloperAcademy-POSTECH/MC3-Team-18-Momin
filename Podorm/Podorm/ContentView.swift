df7baf44c2bd8cb8b1b4884a71c9cb48e3e9ccd6
HoJong <pjh00098@gmail.com>
2022-08-25 11:54:08 +0900

    Chore: Add New SwiftUI Project

---
+++ Podorm/Podorm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Podorm
+//
+//  Created by JongHo Park on 2022/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Podorm/Podorm/PodormApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodormApp.swift
+//  Podorm
+//
+//  Created by JongHo Park on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PodormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
