eabb567487869241f8e8090c43200be0edf07ba2
Taekhwan Han <hth3396@gmail.com>
2022-09-19 17:02:50 +0900

    Refact: #46 csvConverter 불필요 코드 제거

--- Podorm/Podorm/Interactors/DocumentPickerInteractor.swift
+++ Podorm/Podorm/Interactors/DocumentPickerInteractor.swift
@@ -34,7 +34,7 @@ extension RealDocumentPickerInteractor: DocumentPickerInteractor {
         csvRows.removeFirst()
         for csvRow in csvRows {
             let csvColumns = csvRow.components(separatedBy: ",")
-            let studentEntity = Student.init(Int(csvColumns[1]) ?? 0, csvColumns[0], csvColumns[0], Int(csvColumns[2]) ?? 0, 0)
+            let studentEntity = Student(Int(csvColumns[1]) ?? 0, csvColumns[0], csvColumns[0], Int(csvColumns[2]) ?? 0, 0)
             // FIXME: .csv파일에 따라 init 매개변수 변경예정 현재는 MockData_CSV.csv 에 따름
             dormStudentData.append(studentEntity)
         }
